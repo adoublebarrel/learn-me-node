82c5dbb87a060f45142f286d46afe0988b697abb
Alexander Bransby-Williams <alex.bransbywilliams@gmail.com>
2015-01-23 12:32:46 +0000

    Started te stream-adventure challenges/tutorials.

---
+++ stream-adventure/transform.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var through = require('through');
+var upperCaseTransofrm = through(function write(data) {
+		this.queue(data.toString().toUpperCase());
+});
+
+process.stdin
+	.pipe(upperCaseTransofrm)
+	.pipe(process.stdout);
